46679d79a9a56cd82a80f5fcade095a6fe5e49c1
iSage <isage.dna@gmail.com>
2018-01-02 23:33:14 +0300

    Add French, German, Italian and Polish

--- build.sh
+++ build.sh
@@ -4,12 +4,16 @@ mkdir data/lang
 wget https://github.com/nxengine/tsc-converter/releases/download/v1.0/tsc.tar.gz
 tar -zxf tsc.tar.gz
 
-wget https://github.com/nxengine/nx-fontgen/releases/download/v1.1/fontbm.tar.gz
+wget https://github.com/nxengine/nx-fontgen/releases/download/v1.2/fontbm.tar.gz
 tar -zxf fontbm.tar.gz
 
 
 git clone https://github.com/nxengine/lang_russian
 git clone https://github.com/nxengine/lang_japanese
+git clone https://github.com/nxengine/lang_french
+git clone https://github.com/nxengine/lang_polish
+git clone https://github.com/nxengine/lang_german
+git clone https://github.com/nxengine/lang_italian
 
 
 for i in lang*
